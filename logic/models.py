4979d964c95cc6362f89a0753d79bef89f95fe0b
Hanan-Hussein <hanhuss035@gmail.com>
2022-06-20 15:56:55 +0300

    ft:models-created neighbourhood model

--- logic/models.py
+++ logic/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=50, null=False, unique=True)
+    location = models.CharField(max_length=50, null=False)
+    police = models.IntegerField(null=True)
+    health = models.IntegerField(null=True)
+    handyman = models.IntegerField(null=True)
+    # occupants = models.IntegerField(default=0)
+    admin = models.ForeignKey(
+        User, null=False, related_name='neighbourhoods', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
